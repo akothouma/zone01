c2b0169b90efa59a304c48fa345143038afed5ca
lakoth <lornaakothpb@gmail.com>
2023-10-25 09:39:37 +0300

    script that ignores hidden files and lists files and directories according to recently timed files formatted with comma between them third commit

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,4 +1,4 @@
  #! /bin/bash
 
- curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json |jq '.[]|select(.id==70).name'
+ curl https://learn.zone01kisumu.ke/assets/superhero/all.json |jq '.[]|select(.id==70).name'
  
