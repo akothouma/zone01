31727e31a81045aad032fc408ee9e4ab52de8ae2
lakoth <lornaakothpb@gmail.com>
2023-10-25 20:53:47 +0300

    alpha

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,10 +1,9 @@
 package main
 
-import "github.com/01-edu/z01"
+import  "github.com/01-edu/z01"
 
-func main()
-{
-	for c := 'a'; c <= 'z' ; c++ {
+func main() {
+	for c := 'a'; c <= 'z'; c++ {
        z01.PrintRune(c)
 	}
 	z01.PrintRune('\n')
