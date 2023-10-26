e509b310d1e81fc63c8448be9ef97a77ae8dd423
lakoth <lornaakothpb@gmail.com>
2023-10-27 01:05:21 +0300

    prints two unique numbers in ascending order seperated by comma and space fifth commit

--- printcomb2.go
+++ printcomb2.go
@@ -4,14 +4,24 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := i + 1; j <= '9'; j++ {
-			z01.PrintRune(i)
-			z01.PrintRune(j)
+		for a := '0'; a <= '9'; a++ {
+			for b := '0'; b <= '9'; b++ {
+				for j := '0'; j <= '9'; j++ {
+					if a < i || (a == i && b < j) {
+						z01.PrintRune(i)
+						z01.PrintRune(a)
+						z01.PrintRune(' ')
+						z01.PrintRune(j)
+						z01.PrintRune(b)
+						if a != '9' || b != '8' || i != '9' || j != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+
+					}
+				}
+			}
 		}
-		if i != '9' {
-			z01.PrintRune(',')
-			z01.PrintRune(' ')
-		}
-		z01.PrintRune('\n')
 	}
+	z01.PrintRune('\n')
 }
