0a4812b9bf8e70dea0b6ef6fada27b9a4f64905b
lakoth <lornaakothpb@gmail.com>
2023-10-27 01:13:15 +0300

    prints two unique numbers in ascending order seperated by comma and space sixth commit

--- printcomb2.go
+++ printcomb2.go
@@ -8,11 +8,11 @@ func PrintComb2() {
 			for i := '0'; i <= '9'; i++ {
 				for j := '0'; j <= '9'; j++ {
 					if a < i || (a == i && b < j) {
-						z01.PrintRune(i)
 						z01.PrintRune(a)
-						z01.PrintRune(' ')
-						z01.PrintRune(j)
 						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						if a != '9' || b != '8' || i != '9' || j != '9' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
