f544e6474c729b4ac6727726a32740bd23c27ddb
lakoth <lornaakothpb@gmail.com>
2023-10-26 21:53:11 +0300

    returns T(true) if number is negative and F(false) if number is positive fifth commit

--- isnegative.go
+++ isnegative.go
@@ -6,7 +6,8 @@ func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
 	}
-	z01.PrintRune('F')
-	z01.PrintRune('\n')
 }
