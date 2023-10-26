8736f962a481efd87ea4372209b6d2dbc94b34a6
lakoth <lornaakothpb@gmail.com>
2023-10-26 23:46:53 +0300

    prints three unique numbers in ascending order seperated by comma and space fourth commit with changes

--- printcomb.go
+++ printcomb.go
@@ -3,16 +3,14 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	a := '0'
-	b := a + 1
-	c := b + 1
-	for ; a <= '7'; a++ {
-		for ; b <= '8'; b++ {
-			for ; c <= '9'; c++ {
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '8'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
 				if a != '7' {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(c)
+
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
