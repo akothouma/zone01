afbb0ba48b90ce60eb0ef5421e3c37557a188fb3
lakoth <lornaakothpb@gmail.com>
2023-10-27 03:48:17 +0300

    prints an int passed in parameter. All possible values of type int go through forth commit

--- printnbr.go
+++ printnbr.go
@@ -3,6 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	var castNumber rune = rune(n)
-	z01.PrintRune(castNumber)
+	var castNumber string = string(n)
+
+	z01.PrintRune('"')
+	for i := 0; i <= len(castNumber)-1; i++ {
+		runecharacter := rune(castNumber[i])
+		z01.PrintRune(runecharacter)
+	}
+	z01.PrintRune('"')
 }
