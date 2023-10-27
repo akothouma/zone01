6571fe24cce77ded9a1fdb647adef081ba4f9052
lakoth <lornaakothpb@gmail.com>
2023-10-27 16:44:29 +0300

    prints characters of a string onr by one fifth  attempt

--- printstr.go
+++ printstr.go
@@ -3,12 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for _, r := range s {
-		if r == '\n' {
+	for _, c := range s {
+		if c == '\n' {
 			z01.PrintRune('\\')
 			z01.PrintRune('n')
 		} else {
-			z01.PrintRune(r)
+			z01.PrintRune(c)
 		}
 	}
 }
