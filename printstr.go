41b29b5d9b7ca53a3bd125c6e2b00f1b3d77f7bc
lakoth <lornaakothpb@gmail.com>
2023-10-27 16:31:20 +0300

    prints characters of a string onr by one third  attempt

--- printstr.go
+++ printstr.go
@@ -5,7 +5,8 @@ import "github.com/01-edu/z01"
 func PrintStr(s string) {
 	for _, r:= range s{
 		if r == '\n'{
-			z01.PrintRune(' ')
+			z01.PrintRune('\\')
+			z01.PrintRune('n')
 		}else{
 			z01.PrintRune(r)
 		}
