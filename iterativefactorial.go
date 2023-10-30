573cc6abc93eade2d3c4d10a7278f01cbfebb0e7
lakoth <lornaakothpb@gmail.com>
2023-10-30 19:41:31 +0300

    function uses recursivity to calculate fuctorial of an integer tenth  attempt

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,15 +2,12 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 0
-
-	for i := 0; i <= nb; i++ {
-		if nb == 0 {
-			return 1
-		} else if nb > 63 || nb < 0 {
-			return 0
-		} else {
-			result = nb * IterativeFactorial(nb-1)
-		}
+	if nb == 0 || nb == 1 {
+		return 1
+	} else if nb > 63 || nb < 0 {
+		return 0
+	} else {
+		result *= nb
 	}
 	return result
 }
