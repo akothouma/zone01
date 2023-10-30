6592b2c3d24e2d5005eeaa8f212c531e3ade7008
lakoth <lornaakothpb@gmail.com>
2023-10-30 18:13:55 +0300

    function uses recursivity to calculate fuctorial of an integer third attempt

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb == -1 || nb <= 0 {
-		return 0
-	} else if nb == 1 {
+	if nb == 1 {
 		return 1
-	} else {
+	} else if nb > 1 {
 		return nb * IterativeFactorial(nb-1)
+	} else {
+		return 0
 	}
 }
