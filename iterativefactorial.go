469a569d1aba86532e576d6ff62fd8962a217dcd
lakoth <lornaakothpb@gmail.com>
2023-10-30 19:11:07 +0300

    function uses recursivity to calculate fuctorial of an integer sixth attempt

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,7 +4,9 @@ func IterativeFactorial(nb int) int {
 	result := 0
 
 	for i := nb; i > 0; nb-- {
-		if nb > 63 || nb < 0 {
+		if nb == 0 {
+			return 1
+		} else if nb > 63 || nb < 0 {
 			return 0
 		} else {
 			result *= nb
