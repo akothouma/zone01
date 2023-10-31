6ba404b6b5e49982045bb1cd843d0d4113aaa1f2
lakoth <lornaakothpb@gmail.com>
2023-10-31 10:35:42 +0300

    uses iteration to find factorial of a number

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,7 +4,7 @@ func IterativeFactorial(nb int) int {
 	result := 1
 	for i := nb; i > 0; i-- {
 		if i == 0 || i == 1 {
-			return 1
+			return result
 		} else if i > 63 || i < 0 {
 			return 0
 		} else {
