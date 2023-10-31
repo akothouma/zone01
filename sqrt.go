859ddac9492ba513775423e5229ab1acb8c9883e
lakoth <lornaakothpb@gmail.com>
2023-10-31 18:38:50 +0300

    finds the square root of a number if the square-root is a whole number third  attempt

--- sqrt.go
+++ sqrt.go
@@ -4,13 +4,11 @@ func Sqrt(nb int) int {
 	if nb == 1 {
 		return 1
 	}
-	if nb%2 == 0 {
-		for i := 2; i*i <= nb; i++ {
-			if i*i == nb {
-				return i
-			} else {
-				return 0
-			}
+	for i := 2; i*i <= nb; i++ {
+		if i*i == nb {
+			return i
+		} else {
+			return 0
 		}
 	}
 	return 0
