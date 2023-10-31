6efb1818b8081e6adb1cc59137507d1946ce835a
lakoth <lornaakothpb@gmail.com>
2023-10-31 18:19:01 +0300

    finds the square root of a number if the square-root is a whole number second attempt

--- sqrt.go
+++ sqrt.go
@@ -1,8 +1,11 @@
 package piscine
 
 func Sqrt(nb int) int {
+	if nb == 1 {
+		return 1
+	}
 	if nb%2 == 0 {
-		for i := 1; i*i <= nb; i++ {
+		for i := 2; i*i <= nb; i++ {
 			if i*i == nb {
 				return i
 			} else {
