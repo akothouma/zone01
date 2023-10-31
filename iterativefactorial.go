0d61c842f5bafafa091e462471daac9ce76b9497
lakoth <lornaakothpb@gmail.com>
2023-10-31 11:16:04 +0300

    uses iteration to find factorial of a number second attempt

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,12 +2,12 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 0; i <= nb; i++ {
-		if i == 0 || i == 1 {
-			return 1
-		} else if i > 63 || i < 0 {
-			return 0
-		} else {
+	if nb < 0 || nb > 63 {
+		return 0
+	} else if nb == 0 || nb == 1 {
+		return 1
+	} else {
+		for i := nb; i > 2; i-- {
 			result = result * i
 		}
 	}
