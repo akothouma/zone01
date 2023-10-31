46124e80ee22fced68301098c243f0d6d48cebaf
lakoth <lornaakothpb@gmail.com>
2023-10-31 13:53:27 +0300

    Finds result of a number raised to a power using recurssion

---
+++ recursivepower.go
@@ -0,0 +1,20 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	var result int
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else if power == 1 {
+		return nb
+	} else {
+		result = nb
+		if nb < 0 && power%2 == 1 {
+			result = -(result * RecursivePower(nb, power-1))
+		} else {
+			result = result * RecursivePower(nb, power-1)
+		}
+	}
+	return result
+}
