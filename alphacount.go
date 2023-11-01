66809a951aa3fbb4200dc5803452cb26512d40bf
lakoth <lornaakothpb@gmail.com>
2023-11-01 19:51:48 +0300

    counts latin alphabet in a string

---
+++ alphacount.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AlphaCount(s string) int {
+	var count int
+	for _, character := range s {
+		if character != '\n' {
+			if character >= 65 && character <= 90 || character >= 97 && character <= 122 {
+				count := 0
+				count++
+			}
+		}
+	}
+	return count
+}
