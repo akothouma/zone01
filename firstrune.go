e9a80e4dbff76e939133055c8667c2cddee76aa0
lakoth <lornaakothpb@gmail.com>
2023-11-01 17:34:08 +0300

    Prints the first rune of a string second attempt

--- firstrune.go
+++ firstrune.go
@@ -1,11 +1,6 @@
 package piscine
 
 func FirstRune(s string) rune {
-	var str rune
-	for i := 0; i <= len(s)-1; i++ {
-		if i == 0 {
-			str = rune(s[0])
-		}
-	}
-	return str
+	i := []rune(s)
+	return i[0]
 }
