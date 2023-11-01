a3eca5dd9d5bf8838a9f6bea180a8e1a3436c179
lakoth <lornaakothpb@gmail.com>
2023-11-01 19:01:40 +0300

    Prints the nth rune of a string sevenths attempt

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	castedstring := []rune(s)
 	index := len(s)
-	if n < index && n < 0 {
+	if n < index && n > 0 {
 		return castedstring[n-1]
 	}
 	return 0
