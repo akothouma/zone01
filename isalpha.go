1d5817d295d3699c3d2d900ec7650e88b3dde0c7
lakoth <lornaakothpb@gmail.com>
2023-11-02 19:08:39 +0300

    checks if the string is only alphanumeric

--- isalpha.go
+++ isalpha.go
@@ -2,8 +2,11 @@ package piscine
 
 func IsAlpha(s string) bool {
 	stringlength := len(s)
+	if stringlength == 0 {
+		return true
+	}
 	for _, character := range s {
-		if stringlength == 0 || character >= 65 && character <= 90 || character >= 97 && character <= 122 {
+		if character >= 65 && character <= 90 || character >= 97 && character <= 122 {
 			continue
 		} else {
 			return false
