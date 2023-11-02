0917f0b55839d6343766ccb3fa9bee275ab92a45
lakoth <lornaakothpb@gmail.com>
2023-11-03 01:50:04 +0300

    changes first character of every word to uppercase letter

--- capitalize.go
+++ capitalize.go
@@ -1,20 +1,19 @@
 package piscine
 
-import "unicode"
-
 func Capitalize(s string) string {
-	var output []rune // create an output slice
-	isWord := true
-	for _, val := range s {
-		if isWord && unicode.IsLetter(val) { // check if character is a letter convert the first character to upper case
-			output = append(output, unicode.ToUpper(val))
-			isWord = false
-		} else if !unicode.IsLetter(val) {
-			isWord = true
-			output = append(output, val)
+	s = ToLower(s)
+	for i, r := range s {
+		if i == 0 {
+			s = ToUpper(string(r)) + s[i+1:]
 		} else {
-			output = append(output, val)
+			if IsAlpha(string(r)) && !IsAlpha(string(s[i-1])) {
+				if i != len(s) {
+					s = s[:i] + ToUpper(string(r)) + s[i+1:]
+				} else {
+					s = s[:i] + ToUpper(string(r))
+				}
+			}
 		}
 	}
-	return string(output)
+	return s
 }
