8ebd38440eb6f3c04c8ecb7b671ef68e276d23b4
lakoth <lornaakothpb@gmail.com>
2023-11-02 20:30:19 +0300

    changes the rune values of a string to upercase

---
+++ toupper.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToUpper(s string) string {
+	var character rune
+	var str string
+	for _, ch := range s {
+		if ch >= 'a' && ch <= 'z' {
+			character = ch + 32
+		}
+		str = str + string(character)
+	}
+	return str
+}
