18b9559ec4104851faf71c7660ea3c5d7c82d1b1
lakoth <lornaakothpb@gmail.com>
2023-11-02 17:15:53 +0300

    checks whether a string contains non uppercase alphabet and returns false if it does

--- isupper.go
+++ isupper.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsUpper(s string) bool {
 	for _, character := range s {
-		if character < 65 && character > 90 {
+		if character < 65 || character > 90 {
 			return false
 		}
 	}
