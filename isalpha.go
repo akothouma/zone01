7d224b3ee08c616403bbef988aa8f06db2b180de
lakoth <lornaakothpb@gmail.com>
2023-11-02 19:25:48 +0300

    checks if the string is only alphanumeric

--- isalpha.go
+++ isalpha.go
@@ -6,7 +6,7 @@ func IsAlpha(s string) bool {
 		return true
 	}
 	for _, character := range s {
-		if character >= 65 && character <= 90 || character >= 97 && character <= 122 {
+		if (character >= 65 && character <= 90) || (character >= 97 && character <= 122) {
 			continue
 		} else {
 			return false
