bc5f32225b8ee556d24e33411874c8b59185fc1b
lakoth <lornaakothpb@gmail.com>
2023-11-02 16:32:41 +0300

    Finds if a substring exists and returns index of first character of substring

--- index.go
+++ index.go
@@ -1,8 +1,8 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	for i := 0; i < len(s); i++ {
-		if s[i:i+len(toFind)-1] == toFind {
+	for i := 0; i < len(s)-len(toFind)+1; i++ {
+		if s[i:i+len(toFind)] == toFind {
 			return i
 		}
 	}
