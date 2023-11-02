e67562bdde49330b3a0ebeeb512bc392a48c5993
lakoth <lornaakothpb@gmail.com>
2023-11-03 00:32:00 +0300

    prints number in ascending order of place values

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -5,15 +5,13 @@ import "github.com/01-edu/z01"
 func PrintNbrInOrder(n int) {
 	var num, remainder int
 
-	result := 0
-
 	if n == 0 {
 		z01.PrintRune('0')
 	} else {
 		for n > 0 {
 			remainder = n % 10
 			num = n / 10
-			result = (result * 10) + remainder
+			// result + =remainder
 			n = num
 			z01.PrintRune(rune(remainder))
 		}
