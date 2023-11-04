46bfeda19fc469ea2ddeebafdb8fc0aa7bec8b6f
lakoth <lornaakothpb@gmail.com>
2023-11-04 19:11:43 +0300

    concats params with newline

--- concatparams.go
+++ concatparams.go
@@ -2,8 +2,12 @@ package piscine
 
 func ConcatParams(args []string) string {
 	var requiredstring string
-	for _, onestring := range args {
-		requiredstring += onestring + "\n"
+	for index, onestring := range args {
+		if index == 0 {
+			requiredstring = args[0]
+		} else {
+			requiredstring += "\n" + onestring
+		}
 	}
 	return requiredstring
 }
