6327201003a9facbdc12d33ba7cdb2f8250c521e
lakoth <lornaakothpb@gmail.com>
2023-11-04 20:30:57 +0300

    removes whitespace

---
+++ splitwhitespaces.go
@@ -0,0 +1,20 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+  var requiredstring []string
+  word:="" //empty string called word
+  for _,ch:=range s{
+	if ch == ' ' || ch == '\t' || ch== '\n'{
+		if word != ""{
+		requiredstring=append(requiredstring, word)
+		word= ""
+		}else {
+		word +=string(ch)
+		}
+	}
+	if word != " "{
+		requiredstring=append(requiredstring, word)
+	}	
+}
+return requiredstring
+	}
