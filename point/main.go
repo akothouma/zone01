39178f77b84b2babbf3e04c7b664646f4d60764a
lakoth <lornaakothpb@gmail.com>
2023-11-08 19:12:43 +0300

    initializes variables of a point structure

--- point/main.go
+++ point/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x, y int
+}
+
+func PrintString(s string) {
+	for _, str := range s {
+		z01.PrintRune(str)
+	}
 }
 
 func setPoint(ptr *point) {
@@ -11,10 +17,28 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintNumRune(i int) {
+	char := '0'
+	for j := 0; j < i; j++ {
+		char++
+	}
+	z01.PrintRune(char)
+}
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
+	a := points.x / 10
+	b := points.x % 10
+	c := points.y / 10
+	d := points.y % 10
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	PrintString("x = ")
+	PrintNumRune(a)
+	PrintNumRune(b)
+	PrintString(", y = ")
+	PrintNumRune(c)
+	PrintNumRune(d)
+	z01.PrintRune('\n')
 }
