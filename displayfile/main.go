e0a7adf8196a8a02b6b873bdba350a59a485597d
lakoth <lornaakothpb@gmail.com>
2023-11-08 20:27:11 +0300

    manipulating files in go

---
+++ displayfile/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arguements := os.Args
+
+	if len(arguements) <= 1 {
+		fmt.Println("File name missing")
+	} else if len(arguements) > 2 {
+		fmt.Println("Too many arguements")
+	} else {
+
+		file, err := os.Open("quest8.txt")
+
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			stringToPrint := make([]byte, 14)
+			_, err := file.Read(stringToPrint)
+
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println(string(stringToPrint))
+			}
+			file.Close()
+		}
+	}
+}
