4c168e5998badcdb21ac410f92361c27509aff77
lakoth <lornaakothpb@gmail.com>
2023-11-08 12:22:35 +0300

    prints EvenMessage if number is even and OddMessage if number is odd

--- boolean/main.go
+++ boolean/main.go
@@ -1,5 +1,7 @@
 package main
-import "github.com/01-edu/z01"
+
+import ("github.com/01-edu/z01"
+"os")
 
 func printStr(s string) {
 	str:=[]rune(s)
@@ -9,18 +11,20 @@ func printStr(s string) {
 	z01.PrintRune('\n')
 }
 
-func isEven(nbr int) bool{
-	if nbr %2 == 1 {
-		return false
-	} else {
+func isEven(nbr int) bool {
+	if nbr %2== 0 {
 		return true
+	} else {
+		return false
 	}
 }
 
 func main() {
-	if isEven(4){
-		printStr("EvenMsg")
+	Arg:=os.Args[1:]
+	lengthOfArg:=len(Arg)
+	if isEven(lengthOfArg) {
+		printStr("I have an even number of arguments")
 	} else {
-		printStr("OddMsg")
+		printStr("I have an odd number of arguments")
 	}
 }
