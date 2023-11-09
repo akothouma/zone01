5c45f7bf5084dd0c87fc1a68e48532f7a363384f
lakoth <lornaakothpb@gmail.com>
2023-11-09 16:24:17 +0300

    deal a pack of cards

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -6,7 +6,7 @@ func DealAPackOfCards(deck []int) {
 	count := 1
 	for b := 0; b < 12; b += 3 {
 
-		fmt.Printf("Player %v:%v,%v,%v \n", count,
+		fmt.Printf("Player %v: %v, %v, %v \n", count,
 			deck[b], deck[b+1], deck[b+2])
 		count++
 	}
