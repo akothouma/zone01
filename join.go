5269703f96aeb2392e8e02241271ded269d06e49
lakoth <lornaakothpb@gmail.com>
2023-11-09 18:48:40 +0300

    joins different strings into one with a delimeter

--- join.go
+++ join.go
@@ -1,11 +1,12 @@
 package piscine
 
 func Join(strs []string, sep string) string {
+	separator := []rune(sep)
 	var joinedstring string
 	for _, str := range strs {
 		for _, char := range str {
 			if char == ',' {
-				char = ':'
+				char = separator[0]
 			}
 			joinedstring += string(char)
 		}
