b239e998c606fdb52c07a585b862b6fdab6a4dfe
lakoth <lornaakothpb@gmail.com>
2023-11-09 16:04:47 +0300

    deal a pack of cards

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,12 +3,13 @@ package piscine
 import "fmt"
 
 func DealAPackOfCards(deck []int) {
-	slicewanted := make([]int, 3)
-	for i := 0; i < len(deck)/len(slicewanted); i++ {
-		b := 0
-		slicewanted = deck[b : b+len(slicewanted)]
+	b := 0
+	count := 0
+	if count < 3 {
+		slicewanted := deck[b : b+3]
 		b += 3
-		i++
-		fmt.Printf("Player %v:%v", i+1, slicewanted)
+		count++
+
+		fmt.Printf("Player %v:%v", count, slicewanted)
 	}
 }
