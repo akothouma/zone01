1aa8f7525db7db95c73e08c4ef8d71bf7d66eb97
lakoth <lornaakothpb@gmail.com>
2023-11-09 15:27:12 +0300

    compacts content of string by removing empty spaces

--- compact.go
+++ compact.go
@@ -1,19 +1,13 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
 func Compact(ptr *[]string) int {
 	count := 0
 	for _, char := range *ptr {
-		if !(char == "") {
+		if char != "" {
 			count++
-			for _, ch := range char {
-				z01.PrintRune(ch)
-			}
-			z01.PrintRune('\n')
+			(*ptr)[count] = char
 		}
 	}
+	(*ptr) = (*ptr)[:count]
 	return count
 }
