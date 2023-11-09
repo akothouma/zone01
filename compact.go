050d7a0e2ce9dfc547569611d33d4aec41427342
lakoth <lornaakothpb@gmail.com>
2023-11-09 14:57:01 +0300

    compacts content of string by removing empty spaces

---
+++ compact.go
@@ -0,0 +1,14 @@
+package piscine
+
+import "fmt"
+
+func Compact(ptr *[]string) int {
+	count := 0
+	for _, char := range *ptr {
+		if !(char == "") {
+			count++
+			fmt.Print(char)
+		}
+	}
+	return count
+}
