bc0cf8cf1863f368644fea363cb36ef13198e612
lakoth <lornaakothpb@gmail.com>
2023-11-09 21:10:05 +0300

    prints values between minimum and maximum excluding min

--- descendappendrange.go
+++ descendappendrange.go
@@ -3,7 +3,7 @@ package piscine
 func DescendAppendRange(max, min int) []int {
 	arrayvalues := []int{}
 	if max > min {
-		for i := max; i > min; i++ {
+		for i := max; i > min; i-- {
 			arrayvalues = append(arrayvalues, i)
 		}
 	} else {
