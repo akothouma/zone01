17a2155531bbec2be8df67f8044012344f640d9c
lakoth <lornaakothpb@gmail.com>
2023-11-10 06:49:39 +0300

    loaf of bread hackerthon task

--- loafofbread.go
+++ loafofbread.go
@@ -2,7 +2,7 @@ package piscine
 
 func LoafOfBread(str string) string {
 	var requiredstring string
-	for i := 0; i < len(str); i++ {
+	for i := 0; i < len(str)-1; i++ {
 		requiredstring += str[i : i+4]
 	}
 	return requiredstring
