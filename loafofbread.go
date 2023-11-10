0b4ba971f8745455c1e8f5d29f40151072c9a9cf
lakoth <lornaakothpb@gmail.com>
2023-11-10 07:24:30 +0300

    loaf of bread hackerthon task

--- loafofbread.go
+++ loafofbread.go
@@ -1,10 +1,26 @@
 package piscine
 
 func LoafOfBread(str string) string {
-	lengthofslice := 5
-	var requiredstring string
-	for i := 0; i < len(str)-1; i += lengthofslice + 1 {
-		requiredstring = str[i:i+lengthofslice] + " "
+	if str == "" {
+		return "\n"
 	}
-	return requiredstring
+	if len(str) < 5 {
+		return "Invalid Output\n"
+	}
+	answer := ""
+	counter := 0
+	for i, value := range str {
+		if value != ' ' && counter != 5 {
+			answer += string(value)
+			counter++
+		} else if counter == 5 {
+			answer += " "
+			counter = 0
+		}
+		if i == len(str)-1 && len(answer) > 0 && answer[len(answer)-1] == ' ' {
+			answer = answer[:len(answer)-1]
+		}
+	}
+	answer += "\n"
+	return answer
 }
