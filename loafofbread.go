9aa9a4f083cad0464042d85aeeb4aeef7445e684
lakoth <lornaakothpb@gmail.com>
2023-11-10 07:02:08 +0300

    loaf of bread hackerthon task

--- loafofbread.go
+++ loafofbread.go
@@ -4,7 +4,7 @@ func LoafOfBread(str string) string {
 	lengthofslice := 5
 	var requiredstring string
 	for i := 0; i < len(str)-1; i += lengthofslice + 1 {
-		requiredstring += str[i:i+lengthofslice] + " "
+		requiredstring = str[i:i+lengthofslice] + " "
 	}
 	return requiredstring
 }
