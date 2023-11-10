c4a47f8e61ea617831b6b79356f76eefcb0e5c9b
lakoth <lornaakothpb@gmail.com>
2023-11-10 06:14:03 +0300

    rock and roll divisibility

--- rockandroll.go
+++ rockandroll.go
@@ -2,20 +2,19 @@ package piscine
 
 func RockAndRoll(n int) string {
 	stringToDisplay := "error: number is negative\n"
-	divisibleby2 := "roll\n"
-	divisibleby3 := "rock\n"
+	divisibleby2 := "rock\n"
+	divisibleby3 := "roll\n"
 	Nondiv := "error: non divisible\n"
 	both := "rock and roll\n"
 
 	if n < 0 {
 		return stringToDisplay
+	} else if n%2 == 0 {
+		return divisibleby2
 	} else if n%2 == 1 && n%3 == 1 {
 		return Nondiv
 	} else if n%2 == 0 && n%3 == 0 {
 		return both
-	} else if n%2 == 0 || n%3 == 0 {
-		return divisibleby2
-	} else {
-		return divisibleby3
 	}
+	return divisibleby3
 }
