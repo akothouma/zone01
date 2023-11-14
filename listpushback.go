35382a5ad88a2377a0011626346f949612fe7007
lakoth <lornaakothpb@gmail.com>
2023-11-14 19:35:26 +0300

    Adding node to end of linkedlist another attempt

--- listpushback.go
+++ listpushback.go
@@ -14,7 +14,6 @@ func ListPushBack(l *List, data interface{}) {
 	newNode := &NodeL{Data: data}
 	if l.Head == nil {
 		l.Head = newNode
-	} else {
-		l.Tail.Next = newNode
 	}
+	l.Tail.Next = newNode
 }
