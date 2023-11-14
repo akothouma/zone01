5f18f9a250285576dc11404bdbf47e57d56fda31
lakoth <lornaakothpb@gmail.com>
2023-11-14 21:09:04 +0300

    accessing value of data stored in last node

---
+++ listlast.go
@@ -0,0 +1,15 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListLast(l *List) interface{} {
+	return l.Tail.Data
+}
--- listsize.go
+++ listsize.go
@@ -1,16 +1,16 @@
 package piscine
 
-type NodeL struct {
+type NodeL1 struct {
 	Data interface{}
 	Next *NodeL
 }
 
-type List struct {
+type List2 struct {
 	Head *NodeL
 	Tail *NodeL
 }
 
-func ListSize(l *List) int {
+func ListSize(l *List2) int {
 	TempNode := l.Head
 	count := 0
 	for TempNode != nil {
