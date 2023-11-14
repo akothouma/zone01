6c9c144ac459c02b02b3e7a456b1b574723c1934
lakoth <lornaakothpb@gmail.com>
2023-11-14 21:32:48 +0300

    accessing value of data stored in last node

--- listsize.go
+++ listsize.go
@@ -1,16 +1,16 @@
 package piscine
 
-type NodeL1 struct {
+type NodeL3 struct {
 	Data interface{}
 	Next *NodeL
 }
 
-type List2 struct {
+type List3 struct {
 	Head *NodeL
 	Tail *NodeL
 }
 
-func ListSize(l *List2) int {
+func ListSize(l *List3) int {
 	TempNode := l.Head
 	count := 0
 	for TempNode != nil {
