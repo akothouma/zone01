8327227a00144fe71e1071c0d9db4242f6648619
lakoth <lornaakothpb@gmail.com>
2023-11-14 22:43:27 +0300

    returns pointer to node at given position

---
+++ listat.go
@@ -0,0 +1,22 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	count := pos - 1
+	traversalcount := 0
+	Head:=l
+
+	for Head != nil {
+		traversalcount++
+		if traversalcount !=count {
+			Head=Head.Next
+		}
+		l=Head
+		return l
+	}
+	return nil
+}
