7c35eb9e6d88a3d3440310398ad9be9aa216a7fa
lakoth <lornaakothpb@gmail.com>
2023-11-15 12:27:19 +0300

    reversing a linkedlist

--- listreverse.go
+++ listreverse.go
@@ -1,24 +1,26 @@
 package piscine
 
-import "fmt"
-
 type NodeL struct {
 	Data interface{}
 	Next *NodeL
 }
 
 type List struct {
-	Head *NodeL
-	Tail *NodeL
+	Head     *NodeL
+	Tail     *NodeL
+	PrevNode *NodeL
+	NextNode *NodeL
 }
 
 func ListReverse(l *List) {
-	var value interface{}
-	if l.Tail != l.Head {
-		ptr := l.Tail
-		value = &NodeL{Data: ptr.Data}
-		ptr.Next.Next = l.Tail
+	l.PrevNode = nil
+	l.NextNode = nil
+	for l.Head != nil {
+		l.NextNode = l.Head.Next
+		l.Head.Next = l.PrevNode
+		l.PrevNode = l.Head
+		l.NextNode.Next = l.PrevNode
+		l.Head = l.Head.Next
 	}
-	value = l.Head.Data
-	fmt.Println(value)
+	l.Head = l.PrevNode
 }
