ffa80537027b82a9aaf5dc1a2e227bc82b969ef5
lakoth <lornaakothpb@gmail.com>
2023-11-17 18:23:02 +0300

    applying a function to root in order traversal

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -2,8 +2,8 @@ package piscine
 
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
-		f(root.Left)
-		f(root)
-		f(root.Right)
+		f(&root.Left)
+		f(&root)
+		f(&root.Right)
 	}
 }
