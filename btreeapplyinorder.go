a262f21c172370dc0d60528c9ca0dd739b348cce
lakoth <lornaakothpb@gmail.com>
2023-11-17 20:27:50 +0300

    inserting node to beginning of binary tree

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -3,7 +3,7 @@ package piscine
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		BTreeApplyInorder(root.Left, f)
-		f(root.Parent)
+		f(root.Parent.Data)
 		BTreeApplyInorder(root.Right, f)
 	}
 }
