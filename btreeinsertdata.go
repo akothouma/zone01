6bdd61ff5cbef2ebc9694815d3ca00ecdf7ef421
lakoth <lornaakothpb@gmail.com>
2023-11-17 16:09:18 +0300

    inserting node to beginning of binary tree

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -6,13 +6,13 @@ type TreeNode struct {
 }
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root.Parent == nil {
+	if root == nil {
 		return &TreeNode{Data: data}
 	}
-	if data < root.Parent.Data {
-		root.Left = BTreeInsertData(root.Parent.Left, data)
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
 	} else {
-		root.Right = BTreeInsertData(root.Parent.Right, data)
+		root.Right = BTreeInsertData(root.Right, data)
 	}
-	return root.Parent
+	return root
 }
