213a0db69b38ec28c34047e8efa5404923a43c8f
lakoth <lornaakothpb@gmail.com>
2023-11-18 13:54:37 +0300

    applies a function to ever node in level order traversal

---
+++ btreeapplybylevel.go
@@ -0,0 +1,12 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error))  {
+	var height int
+	if root !=nil{
+		height=BTreeLevelCount(root)
+
+		for i:=1;i<=height;i++{
+			BTreeApplyByLevel(root,f)
+		}
+	}	
+}
