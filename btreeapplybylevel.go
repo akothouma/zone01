e50295d79eaaef41ba6b2d59164f2950ad7091a1
lakoth <lornaakothpb@gmail.com>
2023-11-18 13:55:40 +0300

    applies a function to ever node in tree

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,12 +1,10 @@
 package piscine
 
-func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error))  {
-	var height int
-	if root !=nil{
-		height=BTreeLevelCount(root)
-
-		for i:=1;i<=height;i++{
-			BTreeApplyByLevel(root,f)
-		}
-	}	
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root != nil {
+		BTreeApplyByLevel(root.Left, f)
+		f(root.Data)
+		BTreeApplyByLevel(root.Right, f)
+	}
 }
+
