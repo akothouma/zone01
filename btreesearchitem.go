e3a65aca55cde36d2441a752540088ad46beb602
lakoth <lornaakothpb@gmail.com>
2023-11-18 08:01:51 +0300

    recursively searching for an element in a binary tree

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -3,10 +3,10 @@ package piscine
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root != nil {
 		if elem < root.Data && root.Left != nil {
-			BTreeSearchItem(root.Left, elem)
+			root.Left = BTreeSearchItem(root.Left, elem)
 			root = root.Left.Parent
 		} else if elem > root.Data && root.Left != nil {
-			BTreeSearchItem(root.Right, elem)
+			root.Right = BTreeSearchItem(root.Right, elem)
 			root = root.Right.Parent
 		}
 		return root
